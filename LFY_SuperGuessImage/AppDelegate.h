c7108f1d6c2e0647024fb778d2264e37c30bf417
JnLuffy <525444891@qq.com>
2017-02-04 15:22:01 +0800

    Initial Commit

---
+++ LFY_SuperGuessImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFY_SuperGuessImage
+//
+//  Created by IOS.Mac on 17/2/4.
+//  Copyright © 2017年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFY_SuperGuessImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFY_SuperGuessImage
+//
+//  Created by IOS.Mac on 17/2/4.
+//  Copyright © 2017年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
